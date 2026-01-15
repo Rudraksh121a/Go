afdfe05d1288fd7cca50bb388996f15f35949cda
rudraksh121a <rudrakshkanungo2022@gmail.com>
2026-01-15 15:48:59 +0530

    maps added

--- 10-Maps/main.go
+++ 10-Maps/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from 10-Maps")
+	fmt.Println("Welocme to Maps")
+
+	language := make(map[string]string)
+	language["js"] = "javaScript"
+	language["Py"] = "Python"
+	language["cpp"] = "c++"
+
+	// fmt.Printf("Type of language are %T \n", language)
+	fmt.Printf("List of All language are %v\n", language)
+
+	for key, value := range language {
+		fmt.Printf("for key %v and value is %v\n", key, value)
+	}
+
 }
